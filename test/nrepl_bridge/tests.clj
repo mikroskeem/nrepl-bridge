914ddb88fd4041313e093da21dcee3ee154f0e45
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-07-03 16:08:42 +0300

    Add unit tests

---
+++ test/nrepl_bridge/tests.clj
@@ -0,0 +1,31 @@
+(ns nrepl-bridge.tests
+  (:use [eu.mikroskeem.clj.nrepl.NreplBridge]
+        [clojure.test]))
+
+(defn new-bridge []
+  (eu.mikroskeem.clj.nrepl.NreplBridge.))
+
+(defn boot [n]
+  (.bootServer n "127.0.0.1" 1337))
+
+(deftest test-start-stop []
+  (let [n (new-bridge)]
+    (do
+        ; By default, server shouldn't be running
+        (is (not (.isRunning n)))
+        ; Boot the server
+        (boot n)
+        ; Server should be running now
+        (is (.isRunning n))
+        ; Stop the server
+        (.stopServer n)
+        ; Server reference should be set to nil now
+        (is (not (.isRunning n))))))
+
+(deftest test-double-start []
+  (let [n (new-bridge)]
+    (do
+      (boot n)
+      (is (.isRunning n))
+      (is (thrown-with-msg? IllegalStateException #"nREPL server is already running"
+                            (boot n))))))
