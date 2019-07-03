642a412b855df5ae5d4b6f193bcbf3b58befed0b
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-07-03 16:08:37 +0300

    Add documentation and fix isRunning returning faulty status after stop

--- src/eu/mikroskeem/clj/nrepl/NreplBridge.clj
+++ src/eu/mikroskeem/clj/nrepl/NreplBridge.clj
@@ -7,19 +7,32 @@
               [isRunning [] boolean]
               [stopServer [] void]]))
 
-(defn -init []
+(defn -init
+  "Initializes the NreplBridge wrapper class"
+  [] 
   [[] (atom {})])
 
-(defn -bootServer [this host port]
-  (swap! (.state this) into {:server (start-server :bind host :port port)}))
+(defn -isRunning [this]
+  "Returns whether nREPL server is running or not"
+  (and
+    (contains? @(.state this) :server)
+    (some? (@(.state this) :server))))
 
-(defn -stopServer [this]
-  (if (contains? @(.state this) :server)
-    (do 
+(defn -bootServer
+  "Boots up the nREPL server on TCP host & port"
+  [this host port]
+  (if (-isRunning this)
+    (throw (IllegalStateException. "nREPL server is already running"))
+    (swap! (.state this) into {:server (start-server :bind host :port port)})))
+
+(defn -stopServer
+  "Shuts down the nREPL server"
+  [this]
+  (if (-isRunning this)
+    (do
       (stop-server (@(.state this) :server))
-      (swap! (.state this) into {}))))
-
-(defn -isRunning [this] (contains? @(.state this) :server))
+      (swap! (.state this) into {:server nil}))
+    (throw (IllegalStateException. "nREPL server is not running"))))
 
 (defn -main [& args]
   (println "nrepl-bridge is a library, not application")
