89b7e1b851ad08d98eede774e77c81f190658b20
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-07-03 23:18:37 +0300

    Remove unneeded do

--- test/nrepl_bridge/tests.clj
+++ test/nrepl_bridge/tests.clj
@@ -10,22 +10,20 @@
 
 (deftest test-start-stop []
   (let [n (new-bridge)]
-    (do
-        ; By default, server shouldn't be running
-        (is (not (.isRunning n)))
-        ; Boot the server
-        (boot n)
-        ; Server should be running now
-        (is (.isRunning n))
-        ; Stop the server
-        (.stopServer n)
-        ; Server reference should be set to nil now
-        (is (not (.isRunning n))))))
+    ; By default, server shouldn't be running
+    (is (not (.isRunning n)))
+    ; Boot the server
+    (boot n)
+    ; Server should be running now
+    (is (.isRunning n))
+    ; Stop the server
+    (.stopServer n)
+    ; Server reference should be set to nil now
+    (is (not (.isRunning n)))))
 
 (deftest test-double-start []
   (let [n (new-bridge)]
-    (do
-      (boot n)
-      (is (.isRunning n))
-      (is (thrown-with-msg? IllegalStateException #"nREPL server is already running"
-                            (boot n))))))
+    (boot n)
+    (is (.isRunning n))
+    (is (thrown-with-msg? IllegalStateException #"nREPL server is already running"
+                          (boot n)))))
